9b3d0333115b80b67985bd7282d9629db54c9961
eps1lon <sebastian.silbermann@vercel.com>
2024-04-30 20:48:05 +0200

    Port create-compiler-aliases to Turbopack

--- packages/next-swc/crates/next-core/src/next_import_map.rs
+++ packages/next-swc/crates/next-core/src/next_import_map.rs
@@ -731,8 +731,28 @@ async fn rsc_aliases(
     if runtime == NextRuntime::Edge {
         if ty.supports_react_server() {
             alias["react"] = format!("next/dist/compiled/react{react_channel}/react.react-server");
+            alias["next/dist/compiled/react"] =
+                format!("next/dist/compiled/react{react_channel}/react.react-server");
+            alias["next/dist/compiled/react-experimental"] =
+                format!("next/dist/compiled/react-experimental/react.react-server");
+            alias["react/jsx-runtime"] =
+                format!("next/dist/compiled/react{react_channel}/jsx-runtime.react-server");
+            alias["next/dist/compiled/react/jsx-runtime"] =
+                format!("next/dist/compiled/react{react_channel}/jsx-runtime.react-server");
+            alias["next/dist/compiled/react-experimental/jsx-runtime"] =
+                format!("next/dist/compiled/react-experimental/jsx-runtime.react-server");
+            alias["react/jsx-dev-runtime"] =
+                format!("next/dist/compiled/react{react_channel}/jsx-dev-runtime.react-server");
+            alias["next/dist/compiled/react/jsx-dev-runtime"] =
+                format!("next/dist/compiled/react{react_channel}/jsx-dev-runtime.react-server");
+            alias["next/dist/compiled/react-experimental/jsx-dev-runtime"] =
+                format!("next/dist/compiled/react-experimental/jsx-dev-runtime.react-server");
             alias["react-dom"] =
                 format!("next/dist/compiled/react-dom{react_channel}/react-dom.react-server");
+            alias["next/dist/compiled/react-dom"] =
+                format!("next/dist/compiled/react-dom{react_channel}/react-dom.react-server");
+            alias["next/dist/compiled/react-dom-experimental"] =
+                format!("next/dist/compiled/react-dom-experimental/react-dom.react-server");
         }
     }
 
